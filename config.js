ecf5994f88da1ad766c0ce0fa65e01e3abcccdd0
Alex <zol22222@mail.ru>
2019-01-23 18:33:15 +0700

    Migrate to neuronex

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DATABASE_URL: 'postgres://postgres:0000@localhost:5432/postgres',
-    UPDATE_URL: '/redstar-systems/hosts/api/update',
+    UPDATE_URL: '/neuronex/hosts/api/update',
 };
